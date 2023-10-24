bfb742d2419c4b37c3df8427c86cb9a861e8f10d
chatreeck7 <chatree.devl@gmail.com>
2023-10-24 18:35:29 +0700

    fix(cors): tid corsssssssss

--- service/grpc-client/client.go
+++ service/grpc-client/client.go
@@ -7,7 +7,6 @@ import (
 
 	pb "rentless-services/service/product-service/product"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 )
@@ -29,13 +28,25 @@ type Product struct {
 	Owner        string  `json:"owner"`
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
-
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"*"}
-	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
-	r.Use(cors.New(config))
+	r.Use(CORSMiddleware())
 
 	// Set up a connection to the gRPC server.
 	conn, err := grpc.Dial(serverAddress, grpc.WithInsecure())
--- service/rental-service/main.go
+++ service/rental-service/main.go
@@ -40,8 +40,26 @@ type Product struct {
 	Amount    float64            `json:"amount" bson:"amount"`
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
+	r.Use(CORSMiddleware())
+	
 	r.GET("/rentals", GetAllRentals)
 	r.GET("/rentals/:id", GetRental)
 	r.POST("/rentals", CreateRental)
